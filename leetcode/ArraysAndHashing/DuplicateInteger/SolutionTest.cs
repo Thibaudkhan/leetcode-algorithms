2cb1e984f9d52a8e9e02a0008b0907fa407ec9f3
Thibaud Eder Roussel Pascual <43219426+Thibaudkhan@users.noreply.github.com>
2024-11-10 00:42:07 +0100

    Initial commit: project structure and basic files

---
+++ leetcode/ArraysAndHashing/DuplicateInteger/SolutionTest.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace DefaultNamespace
+{
+    public class SolutionTest
+    {
+        [Fact]
+        public void If_Duplicate_Value_Return_True()
+        {
+            // Correctement initialiser le tableau d'entiers
+            int[] nums = { 1, 2, 3, 3 };
+
+            // Vérifier si le résultat est correct
+            Assert.Equal(3, 3);
+        }
+    }
+}
