9bb84a3931d78c0e2271d7422ccb43e0b8d47530
Thibaud Eder Roussel Pascual <43219426+Thibaudkhan@users.noreply.github.com>
2024-11-11 15:55:07 +0100

    Add TwoSum

---
+++ leetcode/ArraysAndHashing/TwoSum/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices.JavaScript;
+using DefaultNamespace;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace LeetCode.ArraysAndHashing.TwoSum;
+
+public class Solution : ISolution
+{
+    private readonly ITestOutputHelper _output;
+
+    public Solution(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    public Tuple<int, int> Execute(int[] nums,int target,int typeSolution)
+    {
+        switch (typeSolution)
+        {
+            case 1:
+                return ReturnIndicesOfTwoSumsDico(nums,target);
+            default:
+                return ReturnIndicesOfTwoSumsDico(nums,target);
+
+        }
+    }
+
+    private Tuple<int, int> ReturnIndicesOfTwoSumsDico(int[] nums,int target)
+    {
+
+        Dictionary<int, int> dico = new Dictionary<int, int>();
+
+        for(int i = 0; i < nums.Length;i++)        
+        {
+
+            if (dico.ContainsKey(target - nums[i]))
+            {
+                return Tuple.Create(dico[target - nums[i]],i );
+            }
+            dico[nums[i]] = i;
+
+        }
+       return Tuple.Create(0,0);
+    }
+
+
+  
+   
+}
---
+++ leetcode/ArraysAndHashing/TwoSum/SolutionTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace LeetCode.ArraysAndHashing.TwoSum;
+
+public class SolutionTest
+{
+    private Solution _solution;
+    private readonly ITestOutputHelper _output;
+
+
+    public SolutionTest(ITestOutputHelper output)
+    {
+        _output = output;
+
+        _solution = new Solution(_output);
+    }
+    
+    [Theory]
+    [InlineData(new int[] {2, 7, 11, 15},9,new int[] {0,1})]
+    [InlineData(new int[] {3,2,4},6,new int[] {1,2})]
+    public void Return_Index_For_Sum_When_Equal_To_Target( int[] nums,int target,int[] result)
+    {
+
+        Tuple<int, int> answer = Tuple.Create(result[0], result[1]);
+        
+        _output.WriteLine("Testing for input: [2, 7, 11, 15]");
+
+        var  value = _solution.Execute(nums,target,1);
+        _output.WriteLine(value.ToString());
+
+        Assert.Equal(answer,value);
+    }
+    
+
+}
