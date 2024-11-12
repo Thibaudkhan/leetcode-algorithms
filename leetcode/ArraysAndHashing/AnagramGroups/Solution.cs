695199ae67df0d7c6b044be72ce3e8693b47f886
Thibaud Eder Roussel Pascual <43219426+Thibaudkhan@users.noreply.github.com>
2024-11-12 10:31:50 +0100

    Anagram Group

---
+++ leetcode/ArraysAndHashing/AnagramGroups/Solution.cs
@@ -0,0 +1,111 @@
+﻿using DefaultNamespace;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace LeetCode.ArraysAndHashing.AnagramGroups;
+
+public class Solution : ISolution
+{
+    private readonly ITestOutputHelper _output;
+
+    public Solution(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+    
+    public  List<List<string>> Execute(string[] strs,int typeSolution)
+    {
+        switch (typeSolution)
+        {
+            case 1:
+                return ReturnSubStringAnagram(strs);
+            case 2:
+                return ReturnSubStringAnagramBetter(strs);
+
+            default:
+                return ReturnSubStringAnagram(strs);
+
+        }
+    }
+
+    public List<List<string>> ReturnSubStringAnagramBetter(string[] strs)
+    {
+        var dict = new Dictionary<string, List<string>>();
+
+        for (int i = 0; i < strs.Length; i++)
+        {
+            var str = strs[i];
+            var charArray = new char[26];
+
+            for (int j = 0; j < str.Length; j++)
+            {
+                charArray[str[j] - 'a']++;
+            }
+            
+            var key = new string(charArray);
+            _output.WriteLine("key");
+            _output.WriteLine(key);
+
+            if (!dict.ContainsKey(key))
+            {
+                dict[key] = new List<string>();
+            }
+
+            dict[key].Add(str);
+        }
+
+        return dict.Values.ToList();
+    }
+
+    
+    public  List<List<string>> ReturnSubStringAnagram(string[] strs )
+    {
+
+        Dictionary<string, List<string>> dico = new Dictionary<string, List<string>>();
+
+        List<List<string>> response = new List<List<string>>();
+        foreach (var str in strs)
+        {
+            int[] charNum = new int[26];
+
+            for (int j = 0; j < str.Length; j++)
+            {
+                charNum[str[j] - 'a']++;
+            }
+
+            string key = string.Join("-", charNum);
+
+
+            _output.WriteLine(dico.ToFormattedString());
+
+            if (!dico.TryAdd(key, new List<string> { str }))
+            {
+                dico[key].Add(str);
+
+            }
+
+
+        }
+
+        return dico.Values.ToList();
+
+        
+       
+
+    }
+    
+    
+    private void PrintListOfLists(List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            _output.WriteLine($"List {i}: {list[i]}");
+        }
+    }
+    public static string FormatArray(int[] array)
+    {
+        return "[" + string.Join(", ", array) + "]";
+    }
+
+
+}
---
+++ leetcode/ArraysAndHashing/AnagramGroups/SolutionTest.cs
@@ -0,0 +1,109 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+using DefaultNamespace;
+
+namespace LeetCode.ArraysAndHashing.AnagramGroups;
+
+public class SolutionTest
+{
+    private Solution _solution;
+    private readonly ITestOutputHelper _output;
+
+    public SolutionTest(ITestOutputHelper output)
+    {
+        _output = output;
+
+        _solution = new Solution(output);
+    }
+    
+    [Theory]
+    [MemberData(nameof(GetTestData))]
+    public void Return_InTo_SubList_Anagram(string[] strs, List<List<string>> expected)
+    {
+        
+
+        var res = _solution.Execute(strs,1);
+        //_output.WriteLine(res.ToFormattedString());
+        PrintListOfLists(res);
+        _output.WriteLine("res.ToString()");
+
+        Assert.True(AreEqualIgnoringOrder(expected,res));
+    }
+    
+    public static IEnumerable<object[]> GetTestData()
+    {
+        yield return new object[]
+        {
+            new string[] {"act", "pots", "tops", "cat", "stop", "hat"},
+            new List<List<string>>
+            {
+                new List<string> { "hat" },
+                new List<string> { "act", "cat" },
+                new List<string> { "stop", "pots", "tops" }
+            }
+        };
+        yield return new object[]
+        {
+            new string[] {"bdddddddddddd","bdd", "bbbbbbbbbbbbc"},
+            new List<List<string>>
+            {
+                new List<string> { "bbbbbbbbbbbbc" },
+                new List<string> { "bdddddddddddd"},
+                new List<string> { "bdd"}
+            }
+        };
+
+        yield return new object[]
+        {
+            new string[] {"x"},
+            new List<List<string>>
+            {
+                new List<string> { "x" }
+            }
+        };
+        yield return new object[]
+        {
+            new string[] {""},
+            new List<List<string>>
+            {
+                new List<string> { "" }
+            }
+        };
+    }
+    
+    private static bool AreEqualIgnoringOrder(List<List<string>> list1, List<List<string>> list2)
+    {
+        // Si les tailles sont différentes, les listes ne sont pas égales
+        if (list1.Count != list2.Count)
+            return false;
+
+        // Trier chaque sous-liste et la liste principale
+        var sortedList1 = list1
+            .Select(subList => subList.OrderBy(x => x).ToList())
+            .OrderBy(subList => string.Join(",", subList))
+            .ToList();
+
+        var sortedList2 = list2
+            .Select(subList => subList.OrderBy(x => x).ToList())
+            .OrderBy(subList => string.Join(",", subList))
+            .ToList();
+
+        // Comparer chaque sous-liste
+        for (int i = 0; i < sortedList1.Count; i++)
+        {
+            if (!sortedList1[i].SequenceEqual(sortedList2[i]))
+                return false;
+        }
+
+        return true;
+    }
+    
+    public void PrintListOfLists(List<List<string>> listOfLists)
+    {
+        for (int i = 0; i < listOfLists.Count; i++)
+        {
+            _output.WriteLine($"List {i}: [{string.Join(", ", listOfLists[i])}]");
+        }
+    }
+
+}
